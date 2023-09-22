b0b2bbec36b34303b84b6939069b290c47582bb7
Majahenkhosi <411021340@gms.ndhu.edu.tw>
2023-09-22 22:44:28 +0800

    ascii and datatypes

---
+++ ascii.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+  
+  char c1, c2, c3;
+  cout << "Enter 3 letters" <<endl;
+  cin >> c1 >> c2 >> c3 ;
+  cout << "ASCII message: " << int(c1) << " " << int(c2) << " " << int(c3);
+ 
+ return 0;
+}
--- datatypes.cpp
+++ datatypes.cpp
@@ -1,8 +1,26 @@
 #include<iostream>
 
 using namespace std;
+/*
+int main () {
+  int yearOfBirth = 1999;
+  char gender = 'f';
+  bool isOlder = true;
+  float averageGrade = 80.0;
+  double balance = 2000.00;
+
+  cout << sizeof(int) << " bytes\n" << endl; 
+  cout << sizeof(unsigned int) << " bytes\n" << endl; 
+ 
+ return 0;
+}
+
+*/
 
 int main () {
- 
+  
+  int intMax = INT16_MAX;
+  cout << intMax <<endl;
+  
  return 0;
 }
